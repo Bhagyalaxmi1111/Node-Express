58b288ea3ef5c8f98d119d85f64275f9fd881617
Bhagyalaxmi1111 <88611106+Bhagyalaxmi1111@users.noreply.github.com>
2023-06-21 14:59:35 +0530

    contact route
    Dipesh Malvia : https://www.youtube.com/playlist?list=PLTP3E5bPW796_icZanMqhdg7i0Cl7Y51F

---
+++ server.js
@@ -0,0 +1,10 @@
+// console.log("I am in express project");
+const express=require("express");
+const dotenv=require("dotenv").config();
+const app=express();
+const port=process.env.PORT || 5000;
+app.use ("/api/contacts",require("./routes/contactsRoutes"));
+app.listen(port,()=>{
+    console.log(`server running on port ${port}`);
+    
+})
